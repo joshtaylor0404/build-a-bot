600449d40f1068936394374e5bfa3122aba4fb5b
Josh Taylor <Josh_Taylor@esri.com>
2019-05-17 18:24:16 -0700

    directives

--- src/components/build/PartSelector.vue
+++ src/components/build/PartSelector.vue
@@ -3,11 +3,13 @@
     <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
+
+import pinDirective from '../shared/pin-directive';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -20,6 +22,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  directives: { pin: pinDirective },
   data() {
     return { selectedPartIndex: 0 };
   },
@@ -76,9 +79,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
